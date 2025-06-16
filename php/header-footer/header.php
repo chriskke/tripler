c1ec1180fcaf5cc99954a5b2b95b579f7c2b9e90
chriskke <chris552611@gmail.com>
2025-06-16 20:18:42 +0800

    GTM tag

--- php/header-footer/header.php
+++ php/header-footer/header.php
@@ -1,4 +1,11 @@
     <head>
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-N27NQXZN');</script>
+        <!-- End Google Tag Manager -->
         <!-- Google tag (gtag.js)
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-PWMH97H1WF"></script>
         <script>
@@ -48,6 +55,10 @@
         
     </head>
     <body data-mobile-nav-style="classic" class="custom-cursor"> 
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N27NQXZN"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
         <!-- start cursor -->
         <div class="cursor-page-inner">
             <div class="circle-cursor circle-cursor-inner"></div>
